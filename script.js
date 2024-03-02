aca6d60738a643641b74cef7dee258f0de9eabdb
mcrawf9394 <mjc9394@gmail.com>
2024-03-01 23:29:20 -0500

    Adding a shell of wha ti think will be need later, still not sure exactly I will connect everything together at the moment.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const nums = []
 const inputs = []
 const numButtonsContainer = document.getElementById('numButtons')
+const operatorButtonsContainer = document.getElementById('operators')
 for (i = 0; i <= 9; i++) {
     const button = document.createElement('button')
     button.textContent = i
@@ -10,3 +11,35 @@ for (i = 0; i <= 9; i++) {
         inputs.push(i)
         })
     }
+const additionButton = document.createElement('button')
+additionButton.className = "operatorButtons"
+additionButton.id = "additionButton"
+operatorButtonsContainer.appendChild(additionButton)
+additionButton.addEventListener('click', addition())
+function addition(){
+
+}
+const subtractionButton = document.createElement('button')
+subtractionButton.className = "operatorButtons"
+subtractionButton.id = "subtractionButton"
+operatorButtonsContainer.appendChild(subtractionButton)
+subtractionButton.addEventListener('click', subtraction())
+function subtraction (){
+
+}
+const divisionButton = document.createElement('button')
+divisionButton.className = "operatorButtons"
+divisionButton.id = "divisionButton"
+operatorButtonsContainer.appendChild(divisionButton)
+divisionButton.addEventListener('click', division())
+function division (){
+
+}
+const multiplicationButton = document.createElement('button')
+multiplicationButton.className = 'operatorButtons'
+multiplicationButton.id = "multiplicationButton"
+operatorButtonsContainer.appendChild(multiplicationButton)
+multiplicationButton.addEventListener('click', multiplication())
+function multiplication (){
+
+}
