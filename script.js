31a57a6aa83e6a2e8d7d34b364c1a12f7cd68773
mcrawf9394 <mjc9394@gmail.com>
2024-03-01 19:42:13 -0500

    work a little bit with creating some of the buttons and hopefully creating an array when one of the buttons is pressed.

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-let firstNum
-let secondNum
+const nums = []
+const inputs = []
+const numButtonsContainer = document.getElementById('numButtons')
+for (i = 0; i <= 9; i++) {
+    const button = document.createElement('button')
+    button.textContent = i
+    button.className = "numButtons"
+    numButtonsContainer.appendChild(button)
+    button.addEventListener('click', i => {
+        inputs.push(i)
+        })
+    }
